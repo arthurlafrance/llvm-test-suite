6649afd9337b233d6a252c688c383e77d1729b45
Arthur Lafrance <alafrance@fb.com>
2021-08-16 21:36:13 -0700

    Modified analysis script for CSV serialization, added MIP call graph & LTO flags

--- utils/prof-benchmark/stats.py
+++ utils/prof-benchmark/stats.py
@@ -1,21 +1,9 @@
-# what info needs to be displayed for the benchmarks?
-    # comparative box plots of the 3 options (base, llvm, mip) at any given opt level (eg Oz)
-    # line graph of avg of various metrics of the 3 options across different opt levels
-    # stats about differences between options, eg "avg mip binary size was 30% less than llvm"
-
-from argparse import ArgumentParser
 from collections import defaultdict
 from pathlib import Path
-from matplotlib import pyplot as plt
 
 import sys
 import json
 import numpy as np
-
-# save to files in the following format within results/:
-    # box plots: boxplots/box-<opt level>-<metric>.png, eg boxplots/box-Oz.png for each opt level
-    # line graphs: graphs/<metric>.png
-    # stats text: stats.txt
 
 class Metric:
     BIN_SIZE = 'bin_size'
@@ -29,12 +17,7 @@ def get_default_filename(opt_level: str, prof_method: str) -> str:
     return f'./{opt_level}/results-{prof_method}.json'
 
 if __name__ == '__main__':
-    parser = ArgumentParser()
-    parser.add_argument('--format', '-f', help='the text format in which to show results', default='txt') # txt, json
-
-    args = parser.parse_args()
-
-    opt_levels = ['O3', 'Os', 'Oz']
+    opt_levels = ['O3', 'Oz']
     prof_methods = ['base', 'llvm', 'mip']
     benchmarks = {}
 
@@ -88,97 +71,44 @@ if __name__ == '__main__':
                 Metric.ADJUSTED_BIN_SIZE : adjusted_bin_sizes,
             }
 
-    results_dir = Path('results')
-    results_dir.mkdir(parents=True, exist_ok=True)
+    data_dir = Path('data')
+    data_dir.mkdir(parents=True, exist_ok=True)
 
-    fmt = args.format
-
-    outfile_path = results_dir / f'stats.{fmt}'
-    avgs = { opt_level : { prof_method : {} for prof_method in prof_methods } for opt_level in opt_levels }
-
-    with outfile_path.open(mode='w') as outfile:
-        if fmt == 'txt':
-            for metric in Metric.ALL:
-                outfile.write(f'{metric}:\n')
-
-                for opt_level in opt_levels:
-                    outfile.write(f'\t{opt_level}:\n')
-
-                    for prof_method in prof_methods:
-                        outfile.write(f'\t\t{prof_method}:\n')
-
-                        data = benchmarks[opt_level][prof_method][metric]
-
-                        avg = np.average(data)
-                        min_val = np.min(data)
-                        max_val = np.max(data)
-                        std = np.std(data)
-
-                        avgs[opt_level][prof_method][metric] = avg
-                        outfile.write(f'\t\t\tavg: {avg}\n\t\t\tstd: {std}\n\t\t\tmin: {min_val}\n\t\t\tmax: {max_val}\n')
-        else: # fmt == 'json'
-            # { metric : { opt_level : { prof_method : { statistic : value, ... }, ... }, ... }
-            outfile.write('{ ')
-
-            for metric in Metric.ALL:
-                outfile.write(f'"{metric}" : {{ ')
-
-                for opt_level in opt_levels:
-                    outfile.write(f'"{opt_level}" : {{')
-
-                    for prof_method in prof_methods:
-                        outfile.write(f'"{prof_method}" : {{')
-
-                        data = benchmarks[opt_level][prof_method][metric]
-
-                        avg = np.average(data)
-                        min_val = np.min(data)
-                        max_val = np.max(data)
-                        std = np.std(data)
-
-                        avgs[opt_level][prof_method][metric] = avg
-                        outfile.write(f'"avg" : {avg}, "std" : {std}, "min" : {min_val}, "max" : {max_val} }}, ')
-                    outfile.write(' }, ')
-                outfile.write(' }, ')
-            outfile.write(' }')
-
-    # line graphs, one per metric, avg only
-    graphs_dir = results_dir / 'graphs'
-    graphs_dir.mkdir(parents=True, exist_ok=True)
-    
     for metric in Metric.ALL:
-        # TODO: better formatting, including title & other stuff like legend
-        plt.title(f'{metric} by Profile Type Across Optimization Levels (O3 -> Os -> Oz)')
+        data_file = data_dir / f'{metric}.csv'
 
-        for prof_method in prof_methods:
-            data = np.zeros((len(opt_levels),))
+        with data_file.open('w') as f:
+            f.write('Optimization Level,Base,LLVM,MIP\n')
 
-            for i, opt_level in enumerate(opt_levels):
-                data[i] = avgs[opt_level][prof_method][metric]
+            for opt_level in opt_levels:
+                avgs = []
 
-            # print(metric, prof_method, opt_level, data)
-            plt.semilogy(data, label=prof_method)
+                for prof_method in prof_methods:
+                    data = benchmarks[opt_level][prof_method][metric]
+                    avg = np.average(data)
+                    avgs.append(str(avg))
+                
+                data_str = ','.join(avgs)
+                f.write(f'{opt_level},{data_str}\n')
 
-        # finish + save plot
-        plt.legend()
-        plt.savefig(graphs_dir / f'{metric}.png')
-        plt.clf()
+    # how should csv data be formatted?
+        # what will it be used for?
+            # calculating stats by prof method, opt level, and metric (avg, min/max, etc)
+            # plot avg per metric & prof method by opt level
+            # basically all i need is the average of any given configuration of (opt level, prof method, metric)
+        # format:
+            # <prof method>,<opt level>,<metric>,values
 
-    # line graphs, one per metric, avg only
-    plots_dir = results_dir / 'plots'
-    plots_dir.mkdir(parents=True, exist_ok=True)
 
-    # box plots, one per opt-level
-    for opt_level in opt_levels:
-        for metric in Metric.ALL:
-            plt.title(f'{metric} at optimization level {opt_level}')
-            plot_data = []
 
-            for prof_method in prof_methods:
-                data = benchmarks[opt_level][prof_method][metric]
-                plot_data.append(data)
 
-            plt.boxplot(plot_data)
-            # plt.legend()
-            plt.savefig(plots_dir / f'{opt_level}-{metric}.png')
-            plt.clf()
+
+
+
+
+
+
+
+
+
+
